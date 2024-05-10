72bb71abd80898c262d9c8156d21ea47109429b4
wilm <frauke.wilm@fau.de>
2024-05-10 11:23:35 +0200

    generate_dataset_json for CATCH

---
+++ nnunetv2/dataset_conversion/Dataset175_Catch.py
@@ -0,0 +1,18 @@
+from batchgenerators.utilities.file_and_folder_operations import *
+from nnunetv2.dataset_conversion.generate_dataset_json import generate_dataset_json
+from nnunetv2.paths import nnUNet_raw
+
+
+
+if __name__ == "__main__":
+    dataset_name = 'Dataset175_Catch'
+
+    imagestr = join(nnUNet_raw, dataset_name, 'imagesTr')
+    imagests = join(nnUNet_raw, dataset_name, 'imagesTs')
+    labelstr = join(nnUNet_raw, dataset_name, 'labelsTr')
+    labelsts = join(nnUNet_raw, dataset_name, 'labelsTs')
+
+    num_train = 97
+
+    generate_dataset_json(join(nnUNet_raw, dataset_name), {0: 'R', 1: 'G', 2: 'B'}, {'background': 0, 'non-tumor': 1, 'tumor': 2, "ignore": 3},
+                          num_train, '.png', dataset_name=dataset_name)
